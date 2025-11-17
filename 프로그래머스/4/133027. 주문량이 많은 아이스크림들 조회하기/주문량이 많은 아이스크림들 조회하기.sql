7641fca3b6e8072075d7ff1e9ed006a2c32a3071
sunwoopark27 <76925408+sunwoopark27@users.noreply.github.com>
2025-11-17 23:44:41 +0900

    [level 4] Title: 주문량이 많은 아이스크림들 조회하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/4/133027. 주문량이 많은 아이스크림들 조회하기/주문량이 많은 아이스크림들 조회하기.sql
@@ -0,0 +1,12 @@
+with summation as (
+    select flavor, sum(total_order) as tot
+    from (
+        select * from first_half
+        union all
+        select * from july
+    ) as u
+    group by flavor
+    order by tot desc
+)
+
+select flavor from summation limit 3;
